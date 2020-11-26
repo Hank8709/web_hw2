6aa407c3739ba313c8ede49b8b87b9f2e23329ef
Hank8709 <71884624+Hank8709@users.noreply.github.com>
2020-11-26 10:20:58 +0800

    Add files via upload

---
+++ clietn.cpp
@@ -0,0 +1,126 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "wsock32.lib") 
+#include <winsock2.h>
+#include<stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+//client端 顯示部分
+int main(void)
+{
+	WSAData wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // 成功回傳 0
+	SOCKET  server;
+	struct sockaddr_in  IP_serv;
+	server = socket(AF_INET, SOCK_STREAM, 0);//IPV4 , TCP 
+	IP_serv.sin_family = AF_INET;//接收IPV4
+	IP_serv.sin_addr.s_addr = inet_addr("127.0.0.1");//IP位址
+	IP_serv.sin_port = htons(8080);//port號碼
+	connect(server, (LPSOCKADDR)&IP_serv, sizeof(IP_serv));//連線
+	//下棋部分
+	//send(server, "123", 4, 0);
+
+
+	char another_game = 'Y';
+	char game[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+	bool win = true;
+	int choose_player = 0;
+	char number =  '0'  ;
+	int row = 0, col = 0;
+	bool take_turns = false;
+	char player1[20], player2[20];
+	int c;
+
+	do {
+		//初始化遊戲
+		printf("井字棋遊戲初始化...\n");
+		char game[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+		win = false;
+
+		//輸入玩家1,2姓名
+		printf("(不要使用空格,回車鍵結束)\n");
+		printf("請輸入玩家1姓名:\n");
+		scanf("%s", player1);
+		//send(server, player1, sizeof(player1), 0);
+		printf("請輸入玩家2姓名:\n");
+		scanf("%s", player2);
+		send(server, player2, sizeof(player2), 0);
+
+		//隨機選擇先落子玩家
+		srand((unsigned)time(NULL));
+		choose_player = rand() % 2 + 1;
+		if (choose_player == 1) {
+			take_turns = true;
+			printf("%s(玩家1)首先落子(1~9).\n", player1);
+		}
+		else {
+			take_turns = false;
+			printf("%s(玩家2)首先落子(1~9).\n", player2);
+		}
+		while (!win)
+		{
+			//輸出3x3遊戲格
+			printf("\n");
+			printf(" %c | %c | %c \n", game[0][0], game[0][1], game[0][2]);
+			printf("---+---+---\n");
+			printf(" %c | %c | %c \n", game[1][0], game[1][1], game[1][2]);
+			printf("---+---+---\n");
+			printf(" %c | %c | %c \n", game[2][0], game[2][1], game[2][2]);
+			printf("\n");
+
+
+			//玩家1,玩家2分別下棋
+			//落子是否有效
+			do {
+				printf("%s落子:", take_turns ? player1 : player2);
+				//這裡需要清楚緩衝區(why?)
+				if (feof(stdin) || ferror(stdin)) {
+					break;
+				}
+				while ((c = getchar()) != '\n' && c != EOF);
+				scanf("%c",&number);
+				send(server, &number, sizeof(number), 0);//????????????如何send正確的char字元給server
+
+				//why:ASCII碼錶對應的數字差48~
+				row = ((int)number - 48 - 1) / 3;
+				col = ((int)number - 48 - 1) % 3;
+				//printf("%d.%d.%c.\n",row,col,game[row][col]);
+				//printf("%c\n",number);
+			} while (game[row][col] != number);
+
+			//更新棋盤
+			if (take_turns) {
+				game[row][col] = 'X';
+			}
+			else {
+				game[row][col] = 'O';
+			}
+			if (take_turns) {
+				take_turns = false;
+			}
+			else {
+				take_turns = true;
+			};
+			/*char win_socket[5];
+			recv(server, win_socket, sizeof(win_socket), 0);	
+			if (win_socket[0] == '1')
+				win = true;
+			else
+				win = false;*/
+		}
+		
+	}
+while (toupper(another_game) == 'Y');
+	closesocket(server); 
+	return 0;
+}
+	
+
+		
+
+	
+	
---
+++ server.cpp
@@ -0,0 +1,95 @@
+#pragma comment(lib, "wsock32.lib") 
+#include <winsock2.h>
+#include<stdio.h>
+
+//server端 判斷輸贏
+int main(void)
+{
+	WSAData wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // 成功回傳 0
+	SOCKET client[2], server;
+	struct sockaddr_in IP_cli, IP_serv;
+	server = socket(AF_INET, SOCK_STREAM, 0);//IPV4 , TCP 
+	IP_serv.sin_family = AF_INET;//接收IPV4
+	IP_serv.sin_addr.s_addr = 0;//IP位址
+	IP_serv.sin_port = htons(8080);//port號碼
+	bind(server, (LPSOCKADDR)&IP_serv, sizeof(IP_serv));//server,上述結構設定好bind在一起,設定大小
+	listen(server, 5);//數字只是一個監聽狀態
+	int len_of_client = sizeof(IP_cli);
+
+	char  player[10][1024];
+
+	int people = 0;
+	//用multithread去控制有幾個玩家連線進來開棋局
+	while (people<1)//看有幾個玩家連線進來,上限2個
+	{
+		
+		client[people] = accept(server, (LPSOCKADDR)&IP_cli, &len_of_client);//監聽到的時候連接近來
+		recv(client[people],player[people],sizeof(player[people]),0);
+		printf("cleint_id %d %s\n", people,player[people]);
+		people++;
+	}
+
+	char position[1024];
+	int round = 0;
+	bool win = false;
+	char game[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+	int row = 0, col = 0;
+	while (!win)//判斷遊戲輸贏,單一棋局
+	{
+		if(round%2==0)
+		{ 
+			recv(client[0], position, sizeof(position), 0);//玩家1下這步棋
+			row = ((int)position[1] - 48 - 1) / 3;
+			col = ((int)position[1] - 48 - 1) % 3;
+			//round++;
+		}
+		else
+		{
+			recv(client[1], position, sizeof(position), 0);//玩家2下這步棋
+			row = ((int)position[1] - 48 - 1) / 3;
+			col = ((int)position[1] - 48 - 1) % 3;
+			round++;
+		}
+		if (round % 2 == 0) {
+			game[row][col] = 'X';
+			round++;
+		}
+		else {
+			game[row][col] = 'O';
+			round++;
+		}
+			
+		if ((game[0][0] == game[0][1] && game[0][1] == game[0][2]) ||
+			(game[1][0] == game[1][1] && game[1][1] == game[1][2]) ||
+			(game[2][0] == game[2][1] && game[2][1] == game[2][2]) ||
+			(game[0][0] == game[1][0] && game[1][0] == game[2][0]) ||
+			(game[0][1] == game[1][1] && game[1][1] == game[2][1]) ||
+			(game[0][2] == game[1][2] && game[1][2] == game[2][2]) ||
+			(game[0][0] == game[1][1] && game[1][1] == game[2][2]) ||
+			(game[0][2] == game[1][1] && game[1][1] == game[2][0])
+			) {
+			win = true;
+			
+			
+		}
+		if(win == true)
+		{
+			send(client[0], "1", sizeof(char), 0);
+		}
+		else
+			send(client[0], "0", sizeof(char), 0);
+		
+	}
+	
+	//client連進來了
+
+	//遊戲
+
+	closesocket(server);
+
+	for(int i = 0 ;i < 2 ;i++)
+	closesocket(client[i]);
+
+	return 0;
+}
